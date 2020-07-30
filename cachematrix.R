9f559f81c060679dc6510d21c0f2af25e8901e6a
shivamrajput19 <rajputshivam1415@gmail.com>
2020-07-30 16:09:08 +0530

    done

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix
 
-## Write a short comment describing this function
+## This function creates a special kind of a matrix that can cache itself so as
+## to help us take advantages of the lexical skoping in R
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y){
+        x <<- y
+        m <<- NULL
+    }
+    get <- function()x
+    setresolver <- function(solve) m <<- solve
+    getresolver <- function() m
+    list(set = set,
+         get = get,
+         setresolver = setresolver,
+         getresolver = getresolver)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve
+## This function creates the inverse of the cached matrix created above
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getresolver()
+    if(!is.null(m)) {
+        message("obtener datos en caché")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setresolver(m)
+    m
 }
